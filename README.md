9dd0097d2dc718c5f404de87b91d5f8377f3dab2
Alex <alex89.conti@gmail.com>
2022-04-10 11:47:45 +0200

    Fixed setup script

---
+++ README.md
@@ -0,0 +1,14 @@
+# GOVWA - GO Vulnerable Web Application
+
+## Requirements
+1. Local instance of MariaDB (scripts and code refers to `127.0.0.1:3306`)
+2. Environmental variables
+    a. `MYSQL_ROOT_PASSWORD`
+    b. `MYSQL_GOVWA_PASSWORD`
+
+## Setup
+Run script `setup.sh`  
+It will
+1. Hash the `MYSQL_GOVWA_PASSWORD` password
+2. Create `govwauser`, grantings basic privileges
+3. Import a fictitious table, with most common usernames and password
--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
 
-sed 's/PASSWORDPLACHOLDER/'"$MYSQL_GOVWA_PASSWORD"'/g' user_creation.sql | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < create_users_table.sql
+echo "Computing hashed password from cleartext one for user creation"
+HASHED_PASSWORD=`mysql -s -u root -p"$MYSQL_ROOT_PASSWORD" --host=127.0.0.1 --port=3306 -e "select password('"$MYSQL_GOVWA_PASSWORD"');" | tail`
+sed 's/PASSWORDPLACEHOLDER/'$HASHED_PASSWORD'/g' user_creation.sql | mysql --host=127.0.0.1 --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD"
+if [[ "$?" -eq "0" ]]; then
+    echo "User creation: OK"
+    echo "Importing database"
+    mysql --host=127.0.0.1 --port=3306 -u root -p"$MYSQL_ROOT_PASSWORD" < create_users_table.sql
+    if [[ "$?" -eq "0" ]]; then
+        echo "Database import: OK"
+        echo "Exiting (0)"
+        exit 0
+    else
+        echo "Database import: KO"
+        echo "Exiting (2)"
+        exit 2a
+    fi
+else
+    echo "User creation: KO"
+    echo "Exiting (1)"
+    exit 1
+fi
